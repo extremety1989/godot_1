e4b93addff943a399a0e1af08279e847898cb446
extremety1989 <andreazure1989@hotmail.fr>
2022-09-19 18:58:04 +0200

    Update Root.gd

--- Root.gd
+++ Root.gd
@@ -1,8 +1,16 @@
+# client.gd
 extends Node
 
+var udp := PacketPeerUDP.new()
+var connected = false
+
 func _ready():
-	var peer = NetworkedMultiplayerENet.new()
-	peer.create_client("91.162.161.207", 9999)
-	get_tree().network_peer = peer
-	var selfPeerID = get_tree().get_network_unique_id()
-	print(selfPeerID)
+	udp.connect_to_host("91.162.161.207", 32902)
+
+func _process(delta):
+	if !connected:
+		# Try to contact server
+		udp.put_packet("The answer is... 42!".to_utf8())
+	if udp.get_available_packet_count() > 0:
+		print("Connected: %s" % udp.get_packet().get_string_from_utf8())
+		connected = true
