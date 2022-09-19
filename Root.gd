2fd076b608ba12c96d85adf25d19f50482846b59
extremety1989 <andreazure1989@hotmail.fr>
2022-09-19 14:10:07 +0200

    dz

--- Root.gd
+++ Root.gd
@@ -2,4 +2,7 @@ extends Node
 
 func _ready():
 	var peer = NetworkedMultiplayerENet.new()
-	peer.create_client(91162161207, 9999)
+	peer.create_client("91.162.161.207", 9999)
+	get_tree().network_peer = peer
+	var selfPeerID = get_tree().get_network_unique_id()
+	print(selfPeerID)
